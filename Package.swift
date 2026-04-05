614afc8d190c60000c913728585c194bccb36882
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-06 03:12:52 +0800

    [UPDATE] Linux first: bump macOS to v15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "swift-nebula-client",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v15)
     ],
     products: [
         .library(
